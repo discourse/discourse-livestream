b8d671f7f1b6bfd51beaa443cea5f6bfecd10625
Isaac Janzen <isaac.janzen@discourse.org>
2024-05-30 10:08:40 -0600

    DEV: Add requested changes

--- app/models/discourse_livestream/topic_chat_channel.rb
+++ app/models/discourse_livestream/topic_chat_channel.rb
@@ -2,7 +2,7 @@
 
 module DiscourseLivestream
   class TopicChatChannel < ActiveRecord::Base
-    self.table_name = "topic_chat_channels"
+    self.table_name = "livestream_topic_chat_channels"
     belongs_to :topic
     belongs_to :chat_channel, class_name: "Chat::Channel", dependent: :destroy
   end
