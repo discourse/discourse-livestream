c7fcb9b5ad22c1fa1770278d9484f9022e35cfcd
Gabriel Grubba <gabriel@discourse.org>
2024-08-16 15:57:24 -0300

    DEV: change `topic_chat_channel` to `livestream_topic_chat_channel` in chat extension

--- lib/discourse_livestream/chat_channel_extension.rb
+++ lib/discourse_livestream/chat_channel_extension.rb
@@ -5,7 +5,7 @@ module DiscourseLivestream
     extend ActiveSupport::Concern
 
     prepended do
-      has_one :topic_chat_channel,
+      has_one :livestream_topic_chat_channel,
               class_name: "DiscourseLivestream::TopicChatChannel",
               dependent: :destroy,
               foreign_key: :chat_channel_id
