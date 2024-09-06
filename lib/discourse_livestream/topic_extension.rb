5020693d6108161d2e6bd2cb7a4da5e2ffc120b3
Gabriel Grubba <gabriel@discourse.org>
2024-09-06 15:18:03 -0300

    FEATURE: Add update to chatable when topic category changes
    Previously, when a topic category was changed, the chatable was not updated.

--- lib/discourse_livestream/topic_extension.rb
+++ lib/discourse_livestream/topic_extension.rb
@@ -8,6 +8,13 @@ module DiscourseLivestream
       has_one :topic_chat_channel,
               class_name: "DiscourseLivestream::TopicChatChannel",
               dependent: :destroy
+
+      after_update do
+        if topic_chat_channel.present? &&
+             topic_chat_channel&.chat_channel&.chatable_id != category_id
+          topic_chat_channel.chat_channel.update!(chatable_id: category_id)
+        end
+      end
     end
   end
 end
