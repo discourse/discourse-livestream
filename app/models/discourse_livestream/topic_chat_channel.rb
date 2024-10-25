3d6985a39814003cdbaccde9cc2930ab092bf954
Gabriel Grubba <70247653+Grubba27@users.noreply.github.com>
2024-10-25 05:49:07 -0300

    DEV: Update plugin annotations (#36)

--- app/models/discourse_livestream/topic_chat_channel.rb
+++ app/models/discourse_livestream/topic_chat_channel.rb
@@ -7,3 +7,18 @@ module DiscourseLivestream
     belongs_to :chat_channel, class_name: "Chat::Channel", dependent: :destroy
   end
 end
+
+# == Schema Information
+#
+# Table name: livestream_topic_chat_channels
+#
+#  id              :bigint           not null, primary key
+#  topic_id        :integer          not null
+#  chat_channel_id :bigint           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+# Indexes
+#
+#  unique_livestream_topic_chat_channels  (topic_id,chat_channel_id) UNIQUE
+#
